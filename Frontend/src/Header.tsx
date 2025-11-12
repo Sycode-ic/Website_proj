245dc19247d56d328546e9e2a0f2d668fd5e04f5
Om Shanti Limpin <omshantilimpin@gmail.com>
2025-11-12 21:33:26 +0800

    fix: Add padding to the user label in the header component

--- Frontend/src/Header.tsx
+++ Frontend/src/Header.tsx
@@ -8,7 +8,7 @@ const Header = () => {
         <input className="w-4/5 rounded-xl bg-[var(--secondary)] border-1 border-gray-400 text-center h-7" placeholder="Search"></input>
         <div className= "flex flex-row gap-5 w-1/5">
             <FaRegBell className="icon" size={25}/> <FaRegCircleUser className="icon" size={25}/>
-            <p className="font-semibold text-gray-700 text-lg">User</p>
+            <p className="font-semibold text-gray-700 text-lg pr-5">User</p>
         </div>
     </header>
     )
